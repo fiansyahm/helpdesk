a7e6ca2c26fa0d5e6d47b1e02142e78ab5df18bf
fiansyahm <fiansyahmm@gmail.com>
2023-02-15 19:45:28 +0700

    update post

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -10,7 +10,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
   <title>quality assesmen tools</title>
-  <link rel="icon" href='https://i.ibb.co/M8FSKnL/Logo-Izin-Termurah.jpg'>
+  <link rel="icon" href='https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Python.svg/800px-Python.svg.png'>
 <!-- <style>
   th,tr{ 
     display: block; float: left; 
@@ -26,7 +26,7 @@
   <!-- Navbar -->
   <nav class="navbar navbar-dark bg-danger navbar-expand-lg">
     <div class="container-fluid">
-      <img class="mx-3 rounded-circle" src="https://i.ibb.co/M8FSKnL/Logo-Izin-Termurah.jpg" alt="Logo quality asessment" height="40" width="40">
+      <img class="mx-3 rounded-circle" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Python.svg/800px-Python.svg.png" alt="Logo quality asessment" height="40" width="40">
       <a class="navbar-brand" href="/">quality asessment</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,37 @@ Route::get('/', function () {
 });
 
 Route::get('/data/rank', function () {
+    $articles = DB::table('article')
+                ->select('no', 'keywords', 'abstracts', 'year', 'authors', 'citing_new')
+                ->get();
+
+    $data = json_decode($articles, true);
+
+    $result = [];
+
+    $flag=0;
+    foreach ($data as $row) {
+        $flag++;
+        if($flag>25) break;
+        $keywords = preg_split('/\s*,\s*/', $row['keywords']);
+        $authors = preg_split('/\s*,\s*/', $row['authors']);
+        $citingNew = preg_split('/\s*;\s*/', $row['citing_new']);
+        $abstracts = $keywords;
+        $result[] = [$row['no'], $keywords, $abstracts,(string) $row['year'],$authors,  $citingNew];
+    }
+    
     // transporse table
     // https://stackoverflow.com/questions/6297591/how-to-invert-transpose-the-rows-and-columns-of-an-html-table
     $response = Http::post('http://127.0.0.1:5000/data/rank', [
-        'data' => [  
-            [ "a1", ['a','b','c'],   ['a','b','c','k','l']    ,'1993',['p1','p2']                                              ]
-            , [ "a2", ['c','d','e'],   ['a','c','d','e','m','n'],'1993',['p1','p3']                                              ]
-            , [ "a3", ['f','g','h'],   ['c','d','f','g','h','o'],'1993',['p2','p4','p5']                                         ]
-            , [ "a4", ['i','j'],       ['c','d','p','q']        ,'1994',['p3','p6']      ,['a1','a2']                            ]
-            , [ "a5", ['dj','dk'],     ['a','dj','dk','m','r']  ,'1994',['p1','p7']      ,['a1','a2','a3']                       ]
-            , [ "a6", ['d','ac','ad'], ['d','ac','ad','s','t']  ,'1994',['p8','p9']      ,['a1','a3']                            ]
+        'data' => 
+            // $result
+            [  
+                [ "a1", ['a','b','c'],   ['a','b','c','k','l']    ,'1993',['p1','p2']                                              ]
+                , [ "a2", ['c','d','e'],   ['a','c','d','e','m','n'],'1993',['p1','p3']                                              ]
+                , [ "a3", ['f','g','h'],   ['c','d','f','g','h','o'],'1993',['p2','p4','p5']                                         ]
+                , [ "a4", ['i','j'],       ['c','d','p','q']        ,'1994',['p3','p6']      ,['a1','a2']                            ]
+                , [ "a5", ['dj','dk'],     ['a','dj','dk','m','r']  ,'1994',['p1','p7']      ,['a1','a2','a3']                       ]
+                , [ "a6", ['d','ac','ad'], ['d','ac','ad','s','t']  ,'1994',['p8','p9']      ,['a1','a3']                            ]
             ]
     ]);
     // return json_decode($response);
@@ -59,6 +80,25 @@ Route::get('/dashboard', function () {
     $agent = new Agent();
     return view('dashboard', ['agent' => $agent]);
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/db', function () {
+    $articles = DB::table('article')
+                ->select('no', 'keywords', 'abstracts', 'year', 'authors', 'citing_new')
+                ->get();
+
+    $data = json_decode($articles, true);
+
+    $result = [];
+
+    foreach ($data as $row) {
+        $keywords = preg_split('/\s*,\s*/', $row['keywords']);
+        $authors = preg_split('/\s*,\s*/', $row['authors']);
+        $citingNew = preg_split('/\s*;\s*/', $row['citing_new']);
+        $abstracts = $keywords;
+        $result[] = [$row['no'], $keywords, $abstracts,(string) $row['year'],$authors,  $citingNew];
+    }
+    return $result;
+});
 
 
 require __DIR__.'/auth.php';
